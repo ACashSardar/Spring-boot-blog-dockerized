10ceb976f7fe83621214ebdf908d3b9770da42a8
Akash <akashsardar383@gmail.com>
2023-03-14 00:28:32 +0530

    First commit

---
+++ src/main/java/com/akash/blog/entity/Like.java
@@ -0,0 +1,34 @@
+package com.akash.blog.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="likes")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Like {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer likeId;
+	private String likedBy;
+	
+	@ManyToOne
+	@JoinColumn(name="post_id")
+	private Post post;
+	
+	@ManyToOne
+	@JoinColumn(name="user_id")
+	private User user;
+
+}
